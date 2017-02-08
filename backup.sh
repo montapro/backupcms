31c1e56b59a316152221742bcfff416b5755aa04
Markus Krause <montapro@Markuss-MacBook-Pro-3.local>
2017-02-08 17:11:01 +0100

    create timestamp variable

--- backup.sh
+++ backup.sh
@@ -1,12 +1,15 @@
+# timestamp
+timestamp=$( date +%Y-%m-%d-%H-%M-%S )
+
 # variables for mysqldump
 host="host"
 user="user"
-port="port"
+port="3306"
 database="database"
 # dump database
-mysqldump -h $host -u $user -p -P $port --databases $database > $database.sql
+mysqldump -h $host -u $user -p -P $port --databases $database > $database-$timestamp.sql
 
 # variables for zip project folder
 projectFolder="projectfolder"
 # archive project folder
-zip -r $projectFolder.zip $projectFolder
+zip -r $projectFolder-$timestamp.zip $projectFolder
